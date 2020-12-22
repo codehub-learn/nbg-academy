11795c9fc0cfe749ba3a527138140a89e78583eb
dpnevmatikos <pnevmatikos@gmail.com>
2020-12-22 08:37:54 +0200

    Initial commit

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -6,7 +6,7 @@ namespace ConsoleApp2
     {
         static void Main(string[] args)
         {
-            var myVar = "Hello";
+            var myVar = "Hello again";
             Console.WriteLine(myVar);
         }
     }
