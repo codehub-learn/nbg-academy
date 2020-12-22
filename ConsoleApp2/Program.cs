b2efd1108e1892de617c18d8761d54026cf462cd
dpnevmatikos <pnevmatikos@gmail.com>
2020-12-22 08:35:58 +0200

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var myVar = "Hello";
+            Console.WriteLine(myVar);
+        }
+    }
+}
